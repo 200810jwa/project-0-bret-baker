04d4dd3c6ac64579f4b221650f04dce3abc37b7e
Bret Baker <bretbaker808@gmail.com>
2020-08-17 16:41:12 -0400

    started scanner main

--- src/main/java/com/bbbank/scanner/Driver.java
+++ src/main/java/com/bbbank/scanner/Driver.java
@@ -5,8 +5,54 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int selectionInt;
+		boolean auth;
+		
+		System.out.println("Welcome to BB Bank:");
+		System.out.println("Enter 1 for Login / Enter 2 for Sign Up");
+		selectionInt = sc.nextInt();
+		
+		switch(selectionInt) {
+//		Login
+			case 1 :
+				System.out.println("Enter username");
+				String username = sc.next();
+//				System.out.println(username);
+				System.out.println("Enter password");
+				String password = sc.next();
+//				System.out.println(password);
+				
+//				Login Success
+				switch(auth) {
+					case true :
+					
+						break;
+					
+				}
+				
+				break;
+//		Sign Up
+			case 2 :
+				System.out.println("Enter First name");
+				String firstname = sc.next();
+				System.out.println(firstname);
+				System.out.println("Enter Last name");
+				String lastname = sc.next();
+				System.out.println(lastname);
+				System.out.println("Enter email");
+				String email = sc.next();
+				System.out.println(email);
+				System.out.println("Enter 1 for User / Enter 2 for Admin");
+				selectionInt = sc.nextInt();
+				System.out.println(selectionInt);
+				break;
+			default :
+				System.out.println("Error");
+		}
+		
+		
 	}
 
 }

478806fc66a724a32fb6f68f0356e14ca7cf2439
Bret Baker <bretbaker808@gmail.com>
2020-08-18 06:48:45 -0400

    still working on scanner main

--- src/main/java/com/bbbank/scanner/Driver.java
+++ src/main/java/com/bbbank/scanner/Driver.java
@@ -8,32 +8,63 @@ public class Driver {
 		
 		Scanner sc = new Scanner(System.in);
 		int selectionInt;
-		boolean auth;
+		int role;
 		
 		System.out.println("Welcome to BB Bank:");
 		System.out.println("Enter 1 for Login / Enter 2 for Sign Up");
 		selectionInt = sc.nextInt();
 		
+//		switch level 0
 		switch(selectionInt) {
-//		Login
+//		login
 			case 1 :
-				System.out.println("Enter username");
+				System.out.println("Enter Username");
 				String username = sc.next();
 //				System.out.println(username);
-				System.out.println("Enter password");
+//				check db for username, proceed if true, request re-entry if false
+				System.out.println("Enter Password");
 				String password = sc.next();
 //				System.out.println(password);
+//				check db for password, proceed if true, request re-entry if false
 				
-//				Login Success
-				switch(auth) {
-					case true :
-					
+//				if correct credentials entered, get role for db and serve appropriate "homepage"
+//				forced role of user below for development
+				role = 1;
+				
+//				nested switch level 1
+				switch(role) {
+//				user
+					case 1 :
+						System.out.println("Enter 1 to View Balances / Enter 2 to View Account Information / Enter 3 to Logout");
+						selectionInt = sc.nextInt();
+//						System.out.println(selectionInt);
+						
+//					nested switch level 2
+						
+						switch(selectionInt) {
+//						view balances
+							case 1 :
+								break;
+//						view acct info
+							case 2 :
+								break;
+//						logout
+							case 3 :
+								break;
+							default :
+								System.out.println("Error");
+						}
+						
 						break;
-					
+					case 2 :
+						break;
+					default :
+						System.out.println("Error");
 				}
 				
 				break;
-//		Sign Up
+				
+//		sign up
 			case 2 :
 				System.out.println("Enter First name");
 				String firstname = sc.next();

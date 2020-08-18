6ea096aa6a122e24a052100903c57eb20db697e7
Bret Baker <bretbaker808@gmail.com>
2020-08-18 11:11:44 -0400

    finished user balance functionality in scanner main

--- src/main/java/com/bbbank/scanner/Driver.java
+++ src/main/java/com/bbbank/scanner/Driver.java
@@ -20,36 +20,136 @@ public class Driver {
 			case 1 :
 				System.out.println("Enter Username");
 				String username = sc.next();
-//				System.out.println(username);
 //				check db for username, proceed if true, request re-entry if false
 				System.out.println("Enter Password");
 				String password = sc.next();
-//				System.out.println(password);
 //				check db for password, proceed if true, request re-entry if false
 				
 //				if correct credentials entered, get role for db and serve appropriate "homepage"
-//				forced role of user below for development
+//				forced role of user for development
 				role = 1;
 				
 //				nested switch level 1
 				switch(role) {
-//				user
+//				user homepage
 					case 1 :
 						System.out.println("Enter 1 to View Balances / Enter 2 to View Account Information / Enter 3 to Logout");
-						selectionInt = sc.nextInt();
-//						System.out.println(selectionInt);
-						
+						selectionInt = sc.nextInt();						
 //					nested switch level 2
-						
 						switch(selectionInt) {
-//						view balances
+//					view balances
 							case 1 :
+								double checking = 150.62;
+								double savings = 302.76;
+								System.out.println("Checking Balance: $" + checking + " / Savings Balance: $" + savings);
+								System.out.println("");
+								System.out.println("Enter 1 to Make a Deposit / Enter 2 to Withdraw Funds / Enter 3 to Logout");
+								selectionInt = sc.nextInt();								
+//							nested switch level 3
+								switch(selectionInt) {
+//							deposit options
+									case 1 :
+										System.out.println("Enter 1 to Deposit in Checking / Enter 2 to Deposit in Savings / Enter 3 to Logout");
+										selectionInt = sc.nextInt();									
+//									nested switch level 4
+										switch(selectionInt) {
+//									deposit checking
+											case 1 :
+												System.out.println("Enter amount you wish to Deposit");
+												System.out.println("(Amount will round to 2 decimal places / Max Deposit = $1,000.00)");
+												double deposit = sc.nextDouble();
+												System.out.println("Success: $" + deposit + " was added to your Checking account.");
+												double newBalance = checking + deposit;
+												System.out.println("");
+												System.out.println("You have a new Checking balance of $" + newBalance);
+												sc.close();
+												System.out.println("");
+												System.out.println("Thank you for banking with BB Bank");
+												break;
+//									deposit savings			
+											case 2 :
+												System.out.println("Enter amount you wish to Deposit");
+												System.out.println("(Amount will round to 2 decimal places / Max Deposit = $1,000.00)");
+												double deposit2 = sc.nextDouble();
+												System.out.println("Success: $" + deposit2 + " was added to your Savings account.");
+												double newBalance2 = savings + deposit2;
+												System.out.println("");
+												System.out.println("You have a new Savings balance of $" + newBalance2);
+												sc.close();
+												System.out.println("");
+												System.out.println("Thank you for banking with BB Bank");
+												break;
+											case 3 :
+//									logout			
+												sc.close();
+												System.out.println("");
+												System.out.println("Thank you for banking with BB Bank");
+												break;
+											default :
+												System.out.println("Error");
+												break;
+										}									
+										break;
+//							withdraw options	
+									case 2 : 
+//										nested switch level 4
+										switch(selectionInt) {
+//									withdraw checking
+											case 1 :
+												System.out.println("Enter amount you wish to Withdraw");
+												System.out.println("(Amount will round to 2 decimal places / Max Amount = Account Balance)");
+												double withdraw = sc.nextDouble();
+												System.out.println("Success: $" + withdraw + " was withdrawn from your Checking account.");
+												double newBalance = checking - withdraw;
+												System.out.println("");
+												System.out.println("You have a new Checking balance of $" + newBalance);
+												sc.close();
+												System.out.println("");
+												System.out.println("Thank you for banking with BB Bank");
+												break;
+//									withdraw savings			
+											case 2 :
+												System.out.println("Enter amount you wish to Withdraw");
+												System.out.println("(Amount will round to 2 decimal places / Max Amount = Account Balance)");
+												double withdraw2 = sc.nextDouble();
+												System.out.println("Success: $" + withdraw2 + " was withdrawn from your Savings account.");
+												double newBalance2 = savings - withdraw2;
+												System.out.println("");
+												System.out.println("You have a new Savings balance of $" + newBalance2);
+												sc.close();
+												System.out.println("");
+												System.out.println("Thank you for banking with BB Bank");
+												break;
+											case 3 :
+//									logout			
+												sc.close();
+												System.out.println("");
+												System.out.println("Thank you for banking with BB Bank");
+												break;
+											default :
+												System.out.println("Error");
+												break;
+										}									
+										break;
+//							logout
+									case 3 :
+										sc.close();
+										System.out.println("");
+										System.out.println("Thank you for banking with BB Bank");
+										break;
+									default : 
+										System.out.println("Error");
+										break;
+								}
 								break;
 //						view acct info
 							case 2 :
 								break;
 //						logout
 							case 3 :
+								sc.close();
+								System.out.println("");
+								System.out.println("Thank you for banking with BB Bank");
 								break;
 							default :
 								System.out.println("Error");

fffde88d0489be97fbecceb7f185966f0892a049
Bret Baker <bretbaker808@gmail.com>
2020-08-20 07:03:50 -0400

    db connection established, still working on control flow

---
+++ src/main/java/com/bbbank/utils/ConnectionUtil.java
@@ -0,0 +1,48 @@
+package com.bbbank.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+	
+	public static void main(String[] args) {
+		ConnectionUtil.getConnection();
+	}
+
+	private static Connection conn = null;
+	
+	private ConnectionUtil() {
+		super();
+	}
+	
+	public static Connection getConnection() {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				return conn;
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("WE FAILED TO REUSE A CONNECTION");
+			return null;
+		}
+		
+		 String url = System.getenv("DB_URL");
+		 String username = System.getenv("DB_USERNAME");
+		 String password = System.getenv("DB_PASSWORD");
+		
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("DB connected...");
+//			System.out.println(url);
+//			System.out.println(username);
+//			System.out.println(password);
+		} catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("WE FAILED TO GET A CONNECTION!");
+			return null;
+		}
+		
+		return conn;
+	}
+}
